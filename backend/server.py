c57e3a2dc721f790585903591f05b4944d98c4eb
Deep9873 <128281721+Deep9873@users.noreply.github.com>
2025-07-21 23:30:58 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -442,7 +442,9 @@ app.include_router(api_router)
 app.add_middleware(  
     CORSMiddleware,
     allow_credentials=True, 
-    allow_origins=["http://localhost:3000"],
+    allow_origins=
+    ["http://localhost:3000",
+    "https://findmeroom.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
