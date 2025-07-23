92c6b18f502c80c7b5827f7673fce44b4b5486b0
Deep9873 <128281721+Deep9873@users.noreply.github.com>
2025-07-23 14:31:16 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -14,6 +14,8 @@ import hashlib
 import jwt
 from passlib.context import CryptContext
 from fastapi.responses import Response
+from fastapi.responses import FileResponse
+
 
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
